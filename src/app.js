a01b89a751929b77a83866eb5313861063f12f99
ja153903 <jaimeabbariao@gmail.com>
2017-10-24 15:33:20 -0400

    fixing errors with react

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+        This is your starter build.
+      </div>
+    );
+  }
+};
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-};
+import App from './app';
 
 ReactDOM.render(
   <App />
-, document.querySelector('.app'));
+, document.getElementById('app'));
